1a9a49e4b92cafc9239d49778b7bbdbbb7ae8e9e
Jonathan Pinlac <jonppinlac@gmail.com>
2015-12-30 10:40:49 -0500

    Day1 part 1 done. Changed the coords from arrays to objects and had to make a custom indexOf to compare the objects.

--- AOC/day3.js
+++ AOC/day3.js
@@ -1,54 +1,54 @@
-// var data = require('./data');
-// var input = data.d3.split('');
-// var input = ['^','v','^','v','^','v','^','v','^','v'];
-// var input = ['>'];
-var input = ['^','>', 'v','<']
+var data = require('./data');
+var input = data.d3.split('');
+
 var housesReceived = 1;
 var currentLocation = new Coordinates(0,0);
-var visitedLocations = [new Coordinates(currentLocation[0], currentLocation[1])];
-var counter = 0;
+var visitedLocations = [new Coordinates(currentLocation['x'], currentLocation['y'])];
 
 function Coordinates(x,y){
   this.x = x;
   this.y = y;
 }
 
+function findCoord(array, coord) {
+  for (var i = 0; i < array.length; i++){
+    if(array[i].x == coord.x && array[i].y == coord.y){
+      return i;
+    }
+  }
+  return -1;
+}
+
 for (var i = 0; i < input.length; i++) {
-  counter++;
   switch(input[i]){
     case '^':
       currentLocation['x']++;
-      if(visitedLocations.indexOf(currentLocation) === -1){
+      if(findCoord(visitedLocations, currentLocation) === -1){
         housesReceived++;
-        console.log(visitedLocations.indexOf(currentLocation))
       }
-      visitedLocations.push(new Coordinates(currentLocation[0], currentLocation[1]));
+      visitedLocations.push(new Coordinates(currentLocation['x'], currentLocation['y']));
       break;
     case 'v':
       currentLocation['x']--;
-      if(visitedLocations.indexOf(currentLocation) === -1){
+      if(findCoord(visitedLocations, currentLocation) === -1){
         housesReceived++;
-        console.log(visitedLocations.indexOf(currentLocation))
       }
-      visitedLocations.push(new Coordinates(currentLocation[0], currentLocation[1]));
+      visitedLocations.push(new Coordinates(currentLocation['x'], currentLocation['y']));
       break;
     case '>':
       currentLocation['y']++;
-      if(visitedLocations.indexOf(currentLocation) === -1){
+      if(findCoord(visitedLocations, currentLocation) === -1){
         housesReceived++;
-        console.log(visitedLocations.indexOf(currentLocation))
       }
-      visitedLocations.push(new Coordinates(currentLocation[0], currentLocation[1]));
+      visitedLocations.push(new Coordinates(currentLocation['x'], currentLocation['y']));
       break;
     case '<':
       currentLocation['y']--;
-      if(visitedLocations.indexOf(currentLocation) === -1){
+      if(findCoord(visitedLocations, currentLocation) === -1){
         housesReceived++;
-        console.log(visitedLocations.indexOf(currentLocation))
       }
-      visitedLocations.push(new Coordinates(currentLocation[0], currentLocation[1]));
+      visitedLocations.push(new Coordinates(currentLocation['x'], currentLocation['y']));
       break;
   }
-  console.log(visitedLocations)
 }
-console.log(housesReceived)
+console.log(housesReceived);
